eeb4ab0fface479e28fc7eb1ef30464b0505dcb4
Enzo Rocha <139604885+enzorochati@users.noreply.github.com>
2023-11-15 22:38:40 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumeroAleatorio();
+const elementoMenorValor = document.querySelector('#menor-valor');
+const elementoMaiorValor = document.querySelector('#maior-valor');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
+console.log('Número Secreto:', numeroSecreto);
---
+++ assets/js/reconhecimentoDeVoz.js
@@ -0,0 +1,24 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const elementoChute = document.querySelector('#chute');
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+    verificarSeOChutePossuiValorValido(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start());
